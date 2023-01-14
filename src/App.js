b3a9cf7ba5e178b1cad14dcc2414d31eb0b9a544
SanchitDev47 <112927220+SanchitDev47@users.noreply.github.com>
2023-01-14 13:46:03 +0530

    Banner Settings

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import './App.css';
 import { AppBar, Box, Toolbar, Typography, Button } from '@mui/material';
 import Banner from './Components/Banner';
+import AppMenuBar from './Components/AppMenuBar';
 
 function App() {
   return (
     <Box sx={{ flexGrow: 1, }}>
-      <AppBar position="sticky" sx={{ display:'flex', justifyContent:'space-evenly', backgroundColor: 'white', boxShadow: '9', overflowX: 'hidden' }}>
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: '#20C5FA' }}>
-              Sanchit
-            </Typography>
-            <Button>
-              HOME
-            </Button>
-            <Button>
-              SKILLS
-            </Button>
-            <Button>
-              PORTFOLIO
-            </Button>
-            <Button>
-              TESTOMINALS
-            </Button>
-            <Button>
-              CONTACT
-            </Button>
-          </Toolbar>
-      </AppBar>
+      <AppMenuBar />
       <Banner />
     </Box>
   );
--- src/Components/Banner.tsx
+++ src/Components/Banner.tsx
@@ -4,29 +4,34 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import InstagramIcon from '@mui/icons-material/Instagram';
 export default function Banner() {
   return (
-    <>
-      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around', width: '100%', backgroundColor: 'rgb(213,237,251)', background: 'linear-gradient(80deg, rgba(213,237,251,1) 13%, rgba(255,255,255,1) 48%, rgba(213,237,251,1) 81%)', height: '400px' }}>
-        <Grid sx={{ display: 'flex', flexDirection: 'row' }}>
-          <Grid container spacing={3}>
-            <Grid sx={{ width: '60px', fontSize: '4px', display: 'flex', flexDirection: 'column', justifyContent: 'center', gap: '30px' }}>
-              <LinkedInIcon sx={{ fontSize: '19px' }} />
-              <InstagramIcon sx={{ fontSize: '19px' }} />
-              <GitHubIcon sx={{ fontSize: '19px' }} />
-            </Grid>
-            <Grid sx={{ width: '420px', height: '400px', display: 'flex', flexDirection: 'column', justifyContent: 'center', gap: '15px', buttom: '30px' }}>
-              <Typography sx={{ color: 'black', fontSize: '64px', fontWeight: 600 }}>Hi, <Typography component='span' sx={{ fontSize: '64px', fontWeight: 600, color: '#20C5FA' }}>I'm Sanchit</Typography></Typography>
-              <Typography sx={{ fontWeight: 600, fontSize: '24px' }}>FrontEnd | React Developer</Typography>
-              <Typography sx={{ color: 'black', fontSize: '20px', flexWrap: 'wrap' }}>Highly motiveated desgin and developement producing a quality work</Typography>
-              <Grid sx={{gap:'30px'}}>
-                <Button variant="contained">Contact Me</Button>
-              </Grid>
-            </Grid>
+    <Box sx={{
+      width: '100%',
+      height: '450px',
+      display: 'flex',
+      margin: 0,
+      // alignItems: 'center',
+      // justifyContent: 'center',
+      backgroundColor: 'rgb(213,237,251)',
+      background: 'linear-gradient(80deg, rgba(213,237,251,1) 13%, rgba(255,255,255,1) 48%, rgba(213,237,251,1) 81%)',
+    }}>
+      <Grid container spacing={3} sx={{ diplay: 'flex', rowGap: '20px', mt: 0, alignItems: 'center', justifyContent: 'space-evenly' }}>
+        <Grid sx={{ display: 'flex', flexDirection: 'column', gap: '30px' }}>
+          <LinkedInIcon sx={{ fontSize: '19px', cursor: 'pointer' }} />
+          <InstagramIcon sx={{ fontSize: '19px', cursor: 'pointer' }} />
+          <GitHubIcon sx={{ fontSize: '19px', cursor: 'pointer' }} />
+        </Grid>
+        <Grid sx={{ width: '60%', display: 'flex', flexWrap: 'wrap', flexDirection: { lg: 'column', md: 'row' } }}>
+          <Typography sx={{ color: 'black', fontSize: '64px', fontWeight: 600 }}>Hi, <Typography component='span' sx={{ fontSize: '64px', fontWeight: 600, color: '#20C5FA' }}>I'm Sanchit</Typography></Typography>
+          <Typography sx={{ fontWeight: 600, fontSize: '24px' }}>FrontEnd | React Developer</Typography>
+          <Typography variant='caption' sx={{ width: '50%', color: 'black', fontSize: '20px', but: '30px', flexWrap: 'wrap' }}>Highly motiveated desgin and developement producing a quality work</Typography>
+          <Grid>
+            <Button variant="contained">Contact Me</Button>
           </Grid>
         </Grid>
         <Grid>
           <Avatar sx={{ height: '200px', width: '200px' }} alt='sanchit' src='https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80' />
         </Grid>
-      </Box>
-    </>
+      </Grid>
+    </Box>
   )
 }
